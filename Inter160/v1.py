7a9bf975ce33a950f7b48f7a4a04f889b9ea8538
Pengfei <rpfly0818@gmail.com>
2015-07-23 15:09:48 -0400

    Get it

--- Inter160/v1.py
+++ Inter160/v1.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python
+
 class ListNode:
   def __init__(self, x):
     self.val = x
     self.next = None
     
 class Solution:
-  def getIntersection
+  def getIntersection(self, headA, headB):
+    HEADA = headA
+    HEADB = headB
+    while headA != None:
+      while headB != None:
+        if headA.val == headB.val:
+          return headA
+        headB = headB.next
+      headA = headA.next
+      headB = HEADB
+    return None
+          
+# Note that this algorithm is a naive algorithm
+# Submission Result: Time Limit Exceeded
