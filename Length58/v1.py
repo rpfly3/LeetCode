4743055b3a3ff114084ba6dfd59141fdbb9d1e76
Pengfei <rpfly0818@gmail.com>
2015-07-27 21:29:14 -0400

    Get it

---
+++ Length58/v1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+class Solution:
+  # @param {string} s
+  # @return {integer}
+  def lengthOfLastWord(self, s):
+    L = s.split(' ')
+    L.reverse()
+    for i in range(0, len(L)):
+      if len(L[i]) != 0:
+        return len(L[i])
+    return 0
+
+# Note that reversed() function return a strange object not a sequence
+# range(m, n) doesn't include n, from m to n-1
