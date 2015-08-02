c1bc38c0c6be42d47b5e0d172b62885088cdde1c
Pengfei <rpfly0818@gmail.com>
2015-08-02 16:40:29 -0400

    Get it

---
+++ Reverse190/v1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+class Solution:
+  # @param n, an integer
+  # @return an integer
+  def reverseBits(self, n):
+    result = 0
+    for i in range(32):
+      result <<= 1
+      result |= n & 1
+      n >>= 1
+    return result
+
+# Note that this is another important kind of problem
